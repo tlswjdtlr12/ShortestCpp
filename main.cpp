925897c421dd2c334cc45a0b38665275afdb1692
2014004739 <2014004739@hanyang.ac.kr>
2018-12-16 00:46:24 +0900

    finish and code refactoring

--- main.cpp
+++ main.cpp
@@ -1,71 +1,64 @@
-#include <cstdio>
-#include <algorithm>
 #include <queue>
-#include <cstdlib>
 #include <pthread.h>
 #include <vector>
-#include <functional>
 #include <set>
-// compile : g++ -o a.out main.cpp -std=c++11
+// compile : g++ -o a.out main.cpp -std=c++11 -lpthread
 using namespace std;
-#define VER_MAX 10000005
-#define PRO_MAX 100
 #define INF 18446744073709551615LL
 #define CORE_MAX 96
 typedef pair<uint64_t, uint64_t> pairs;
-vector<pairs> edge[VER_MAX];
-uint64_t vt, eg, num_of_problem, hs[PRO_MAX], result[PRO_MAX];
-set<uint64_t> sets[PRO_MAX];
-pthread_t thd[PRO_MAX];
+vector<pairs> edges[10000000];
+uint64_t vt, eg, prbs;
+// 100 is max problem size
+uint64_t hs[100], result[100];
+set<uint64_t> sets[100];
+pthread_t thds[100];
 void * run(void * data);
-
-void addEdge(vector <pairs> adj[], uint64_t u, uint64_t v, uint64_t wt)
+void addEdge(vector <pairs> adjeg[], uint64_t u, uint64_t v, uint64_t wt)
 {
-    adj[u].push_back({v, wt});
-    adj[v].push_back({u, wt});
+    adjeg[u].push_back({v, wt}); adjeg[v].push_back({u, wt});
 }
-
 void threads(uint64_t problem, uint64_t k)
 {
     for(uint64_t i = 0; i<problem; i++)
-        pthread_create(&thd[i], NULL, run, (void*)(i + CORE_MAX*k));
+        pthread_create(&thds[i], NULL, run, (void*)(i + CORE_MAX*k));
     for(uint64_t i = 0; i<problem; i++)
-        pthread_join(thd[i], NULL);
+        pthread_join(thds[i], NULL);
+}
+
+bool checks(uint64_t posit,uint64_t data){
+    if(sets[data].find(posit) != sets[data].end()) return true;
 }
 
 void * run(void * data){
-    uint64_t pnum = (uint64_t)data;
-    for(auto j : sets[pnum]){
+    uint64_t min = INF;
+    for(auto j : sets[(uint64_t)data]){
         priority_queue< pairs, vector<pairs>, greater<pairs> > pq;
+        vector<uint64_t> dist(vt, INF);
+        dist[j] = 0;
         pq.push({0, j});
-        vector<uint64_t> distance(vt, INF);
-        distance[j] = 0;
         while(!pq.empty()){
-            uint64_t now = pq.top().second;
             pq.pop();
-            if(now != j && sets[pnum].find(now) != sets[pnum].end())
+            if(checks(pq.top().second, (uint64_t)data) && pq.top().second != j)
                 break;
-            for (auto x : edge[now]) {
-                uint64_t next = x.first;
-                uint64_t weight = x.second;
-                if (distance[next] > distance[now] + weight) {
-                    distance[next] = distance[now] + weight;
-                    pq.push({distance[next], next});
+            for (auto x : edges[pq.top().second])
+                if (dist[x.first] > dist[pq.top().second] + x.second) {
+                    dist[x.first] = dist[pq.top().second] + x.second;
+                    pq.push({dist[x.first], x.first});
                 }
-            }
         }
-        uint64_t min = INF;
-        for(auto k : sets[pnum]){
+        uint64_t check = (uint64_t)data;
+        for(auto k : sets[check]){
             if(j==k) continue;
-            if(distance[k] < min)
-                min = distance[k];
+            if(dist[k] < min)
+                min = dist[k];
         }
-        if(result[pnum] > min)
-            result[pnum] = min;
+        if(result[check] > min)
+            result[check] = min;
     }
 }
-
 int main(int argc, char ** argv){
+    uint64_t house, m;
     FILE * input = fopen("input.txt", "r");
     FILE * output = fopen("output.txt", "w");
     fscanf(input, "%llu", &vt);
@@ -73,26 +66,23 @@ int main(int argc, char ** argv){
     for(int i = 0; i < eg; i++){
         uint64_t v1, v2, d;
         fscanf(input, "%llu %llu %llu", &v1, &v2, &d);
-        addEdge(edge, v1, v2, d);
+        addEdge(edges, v1, v2, d);
     }
-    fscanf(input, "%llu", &num_of_problem);
-    for(uint64_t i = 0; i < num_of_problem; i++){
+    fscanf(input, "%llu", &prbs);
+    for(uint64_t i = 0; i < prbs; i++){
         fscanf(input, "%llu", &hs[i]);
         for(int j = 0; j < hs[i]; j++){
-            uint64_t temp;
-            fscanf(input, "%llu", &temp);
-            sets[i].insert(temp);
+            fscanf(input, "%llu", &house);
+            sets[i].insert(house);
         }
         result[i] = INF;
     }
-
-    if(num_of_problem <= CORE_MAX) threads(num_of_problem,0);
+    if(prbs <= CORE_MAX) threads(prbs,0);
     else{
-        int m;
-        for(m=0;m<num_of_problem / CORE_MAX; m++) threads(CORE_MAX, m);
-        threads(num_of_problem % CORE_MAX, m);
+        for(m=0;m<prbs / CORE_MAX; m++) threads(CORE_MAX, m);
+        threads(prbs % CORE_MAX, m);
     }
-    for(int i = 0; i < num_of_problem; i++)
+    for(int i = 0; i < prbs; i++)
         fprintf(output, "%llu\n", result[i]);
     fclose(input);
     fclose(output);
